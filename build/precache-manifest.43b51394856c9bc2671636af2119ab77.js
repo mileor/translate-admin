deb18a159deeac027c35ddc784c6365bbf3ec3ce
mileor <mileor@yandex.ru>
2020-02-09 09:23:36 +0300

    fix path

---
+++ build/precache-manifest.43b51394856c9bc2671636af2119ab77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fc8fb804d42617b9dcd218dd37e724",
+    "url": "/translate-admin/index.html"
+  },
+  {
+    "revision": "bee9b3620316fb1f8c63",
+    "url": "/translate-admin/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d832ecfd1dddfc8a3d08",
+    "url": "/translate-admin/static/css/main.ed56fb0d.chunk.css"
+  },
+  {
+    "revision": "bee9b3620316fb1f8c63",
+    "url": "/translate-admin/static/js/2.8faf4955.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/translate-admin/static/js/2.8faf4955.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d832ecfd1dddfc8a3d08",
+    "url": "/translate-admin/static/js/main.d2354a82.chunk.js"
+  },
+  {
+    "revision": "af1299a83eeaffb4c42a",
+    "url": "/translate-admin/static/js/runtime-main.568228d9.js"
+  }
+]);
