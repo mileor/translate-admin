0dd0090d91fc4f7e53258d6b4d53648ab1622375
mileor <mileor@yandex.ru>
2020-02-09 09:07:29 +0300

    init

---
+++ precache-manifest.d53b2815fceb7d2c4a3b6fb49921b15b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed82354a119a44e59408b2fab4a9fa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee9b3620316fb1f8c63",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "319bb59c9ae2d65ddb27",
+    "url": "/static/css/main.ed56fb0d.chunk.css"
+  },
+  {
+    "revision": "bee9b3620316fb1f8c63",
+    "url": "/static/js/2.8faf4955.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8faf4955.chunk.js.LICENSE"
+  },
+  {
+    "revision": "319bb59c9ae2d65ddb27",
+    "url": "/static/js/main.013bf19e.chunk.js"
+  },
+  {
+    "revision": "23a0266d4770b530c0c5",
+    "url": "/static/js/runtime-main.63eda8f3.js"
+  }
+]);
